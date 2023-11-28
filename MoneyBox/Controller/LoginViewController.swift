160aba54ff33c1b37b3af75dfc105022af7d1673
Nuhu Sulemana <turosung@gmail.com>
2023-11-28 15:03:16 +0000

    completed log in screen, created other views and able to fetch data from networking api package

--- MoneyBox/Controller/LoginViewController.swift
+++ MoneyBox/Controller/LoginViewController.swift
@@ -6,7 +6,63 @@
 //
 
 import UIKit
+import Networking
 
 class LoginViewController: UIViewController {
     // Design Login
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+//    override func viewDidLoad() {
+//        view.backgroundColor = .blue
+//
+//    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text,
+            !email.isEmpty else {
+            showAlert(title: "Invalid Email", message: "Email or Username cannot be empty")
+            return
+        }
+        guard let password = passwordTextField.text,
+            !password.isEmpty else {
+            showAlert(title: "Invalid Password", message: "Password cannot be empty")
+            return
+        }
+        
+        let dataProvider = DataProvider()
+        dataProvider.login(request: LoginRequest(email: email, password: password)) { result in
+            switch result {
+            case .success(let success):
+                let user = success.user
+                let session = success.session
+                DispatchQueue.main.async {
+                    self.showAlert(title: "Login Success", message: "Welcome!!")
+                }
+                // Add segue to take user to their Accounts page when login is successful
+                self.performSegue(withIdentifier: "goToAccScreen", sender: self)
+                
+            case .failure(let failure):
+                let errorMessage = failure.localizedDescription
+                DispatchQueue.main.async {
+                    self.showAlert(title: "Login Failed", message: errorMessage)
+                }
+            }
+        }
+
+    }
 }
+
+////MARK: - Code to show alert when logging into app.
+//
+//extension UIViewController {
+//
+//    func showAlert(title: String, message: String) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        let okAction = UIAlertAction(title: "Ok", style: .default)
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+//        alert.addAction(okAction)
+//        alert.addAction(cancelAction)
+//        present(alert, animated: true)
+//    }
+//}
---
+++ MoneyBox/Extensions/Extension+UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+UIViewController.swift
+//  MoneyBox
+//
+//  Created by Nuhu Sulemana on 27/11/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+}
